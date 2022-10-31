d4a64d6446471e42bfbfb7b9abe655aa469715fb
pganesh2k8 <90205289+pganesh2k8@users.noreply.github.com>
2022-10-31 20:03:43 +0530

    Delete providers.tf

--- providers.tf
+++
@@ -1,8 +0,0 @@
-provider "azurerm" {
-  features {}
-
-  client_id       = "4cb93f01-53f4-49d8-b9a4-ce5b7fe7bd3a"
-  tenant_id       = "e59c84a4-af3c-4b74-8a18-02eee8993bfb"
-  subscription_id = "5921a893-7304-44b3-b22f-1bb005522b95"
-  client_secret   = "QZP8Q~luRU83xjCV6tOJ_~Wtni0u5AOVN894LcDF"
-} 
